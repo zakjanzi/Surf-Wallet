9708d58f414938026e803dd3a56d6b34774ab06d
Zak Janzi <82332662+zakjanzi@users.noreply.github.com>
2023-06-22 15:42:29 +0300

    fixed wallet generation (bip39)

--- app/utils/bip39.js
+++ app/utils/bip39.js
@@ -1,38 +1,43 @@
-// const bip39 = require('bip39');
-// const { fromSeed } = require('ethereumjs-wallet/hdkey');
-// const { fromMasterSeed } = require('bitcoinjs-lib');
-// const { HDNode } = require('ethers').utils;
+const bip39 = require('bip39');
+const { ethers } = require('ethers');
+const { HDNode } = require('@ethersproject/hdnode');
 
-// const generateWallet = () => {
-//   // Generate a random mnemonic phrase
-//   const mnemonic = bip39.generateMnemonic();
+const generateWallet = () => {
+  // Generate a random mnemonic phrase
+  const mnemonic = bip39.generateMnemonic();
 
-//   // Derive the master seed from the mnemonic
-//   const masterSeed = bip39.mnemonicToSeedSync(mnemonic);
+  // Derive the master seed from the mnemonic
+  const masterSeed = bip39.mnemonicToSeedSync(mnemonic);
 
-//   // Derive Bitcoin private key
-//   const bitcoinMasterNode = fromMasterSeed(masterSeed);
-//   const bitcoinNode = bitcoinMasterNode.derivePath("m/44'/0'/0'/0");
-//   const bitcoinPrivateKey = bitcoinNode.toWIF();
+  // Derive Bitcoin private key
+  const bitcoinMasterNode = HDNode.fromSeed(masterSeed);
+  const bitcoinNode = bitcoinMasterNode.derivePath("m/44'/0'/0'/0");
+  const bitcoinPrivateKey = bitcoinNode.privateKey;
 
-//   // Derive Ethereum private key
-//   const ethereumMasterNode = fromSeed(masterSeed);
-//   const ethereumNode = ethereumMasterNode.derivePath("m/44'/60'/0'/0");
-//   const ethereumPrivateKey = ethereumNode.getWallet().getPrivateKey();
+  // Derive Ethereum private key
+  const ethereumMasterNode = HDNode.fromSeed(masterSeed);
+  const ethereumNode = ethereumMasterNode.derivePath("m/44'/60'/0'/0");
+  const ethereumPrivateKey = ethereumNode.privateKey;
 
-//   // Example for USDT (Tether) token using Ethereum derivation path
-//   const usdtNode = ethereumMasterNode.derivePath("m/44'/60'/0'/0/0");
-//   const usdtPrivateKey = usdtNode.getWallet().getPrivateKey();
+  // Example for USDT (Tether) token using Ethereum derivation path
+  const usdtNode = ethereumMasterNode.derivePath("m/44'/60'/0'/0/0");
+  const usdtPrivateKey = usdtNode.privateKey;
 
-//   // Return the generated wallet data or perform any other necessary actions
-//   return {
-//     mnemonic,
-//     bitcoinPrivateKey,
-//     ethereumPrivateKey,
-//     usdtPrivateKey,
-//   };
-// };
+  // Return the generated wallet data or perform any other necessary actions
+  return {
+    mnemonic,
+    bitcoinPrivateKey,
+    ethereumPrivateKey,
+    usdtPrivateKey,
+  };
+};
 
-// module.exports = {
-//   generateWallet,
-// };
+const wallet = generateWallet();
+console.log('Mnemonic:', wallet.mnemonic);
+console.log('Ethereum Private Key:', wallet.ethereumPrivateKey);
+console.log('USDT Private Key:', wallet.usdtPrivateKey);
+console.log('Bitcoin Private Key', wallet.bitcoinPrivateKey);
+
+module.exports = {
+  generateWallet,
+};
